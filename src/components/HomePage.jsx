b4d6b2a8ea38428a042a04f7d7f7d85b55c4eae0
starlitnightsky <miraclehourglass@gmail.com>
2023-05-23 13:57:05 +0200

    feat:added link

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import { ThemeContext } from '../App'
 
 function HomePage() {
   const theme = useContext(ThemeContext)
   return (
     <div className='w-full justify-center flex'>
-      <p
+      <h1
         className={`text-5xl font-bold text-center leading-[64px] ${
           theme === 'Default' ? ' text-black' : 'text-red-600'
         }`}
       >
         Welcome to the <br />
         {theme === 'Default' ? 'Qatalog' : theme} homepage!
-      </p>
+      </h1>
+      <Link
+        to='/settings'
+        className={`${
+          theme === 'Default' ? ' text-black' : 'text-red-600'
+        } font-semibold text-lg absolute bottom-4 w-full text-center`}
+      >
+        Back to Settings
+      </Link>
     </div>
   )
 }
