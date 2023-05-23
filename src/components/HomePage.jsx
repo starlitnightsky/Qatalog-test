611dc27b121439e3484cfc04e9e1d25ed54acc50
starlitnightsky <miraclehourglass@gmail.com>
2023-05-23 13:55:40 +0200

    feat:added homepage title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import { ThemeContext } from '../App'
+
 function HomePage() {
-  return <div></div>
+  const theme = useContext(ThemeContext)
+  return (
+    <div className='w-full justify-center flex'>
+      <p
+        className={`text-5xl font-bold text-center leading-[64px] ${
+          theme === 'Default' ? ' text-black' : 'text-red-600'
+        }`}
+      >
+        Welcome to the <br />
+        {theme === 'Default' ? 'Qatalog' : theme} homepage!
+      </p>
+    </div>
+  )
 }
 
 export default HomePage
